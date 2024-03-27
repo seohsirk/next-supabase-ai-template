8f6404488038cf7ca3602183d800243e06974e8f
giancarlo <giancarlopsk@gmail.com>
2024-03-27 14:10:53 +0800

    Refactor mailer setup and validate web configuration in app
    This commit refactors how SMTP configuration for mailers is set up and introduces schema validation for incoming configurations. The mailer modules have been restructured, with schema definition files added, and redundant codes removed. Moreover, web application configuration now has minimum validation on name and title, and URL validation has been added.

--- config/app.config.ts
+++ config/app.config.ts
@@ -8,11 +8,27 @@ enum Themes {
 }
 
 const AppConfigSchema = z.object({
-  name: z.string(),
-  title: z.string(),
-  description: z.string(),
-  url: z.string(),
-  locale: z.string().default('en'),
+  name: z
+    .string({
+      description: `This is the name of your SaaS. Ex. "Makerkit"`,
+    })
+    .min(1),
+  title: z
+    .string({
+      description: `This is the default title tag of your SaaS.`,
+    })
+    .min(1),
+  description: z.string({
+    description: `This is the default description of your SaaS.`,
+  }),
+  url: z.string().url({
+    message: `Please provide a valid URL. Example: 'https://example.com'`,
+  }),
+  locale: z
+    .string({
+      description: `This is the default locale of your SaaS.`,
+    })
+    .default('en'),
   theme: z.nativeEnum(Themes),
   production: z.boolean(),
   themeColor: z.string(),
