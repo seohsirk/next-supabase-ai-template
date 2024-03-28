2f90979761aaeefb593763b57134481a4e7b8e84
giancarlo <giancarlopsk@gmail.com>
2024-03-29 00:38:59 +0800

    Update tailwind config and implement personal account avatar
    In this commit, we have made several updates to tailwind configuration files to ensure better scalability and include exceptions for node modules. Moreover, the account selector component now uses a personalized avatar for each account. This was achieved by accessing personal account data through a hook and introducing a new UserAvatar function to display the avatar. Check component was also migrated to CheckCircle and minor layout and styling adjustments were done for better UI experience.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,6 @@ import baseConfig from '@kit/tailwind-config';
 export default {
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
-  content: [...baseConfig.content, '../../packages/ui/src/**/*.{ts,tsx}'],
+  content: [...baseConfig.content],
   presets: [baseConfig],
 } satisfies Config;
