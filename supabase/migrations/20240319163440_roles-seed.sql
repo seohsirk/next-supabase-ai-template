aee48d2a3be55454a722feaf940722cfbb9d52f0
giancarlo <giancarlopsk@gmail.com>
2024-04-13 17:10:42 +0800

    Update e2e config, package.json, and database schema
    The e2e test configuration has been modified to take screenshots only on failure and the dev server code has been adjusted for better project directory handling. The "supabase:reset" command in package.json no longer starts the server after reset. Default roles 'owner' and 'member' are also now seeded into the roles table, and constraints in the roles table and its associated functions have been updated to manage hierarchy levels and role naming more effectively.

--- supabase/migrations/20240319163440_roles-seed.sql
+++ supabase/migrations/20240319163440_roles-seed.sql
@@ -1,3 +1,18 @@
+-- Seed the roles table with default roles 'owner' and 'member'
+insert into public.roles(
+    name,
+    hierarchy_level)
+values (
+    'owner',
+    1);
+
+insert into public.roles(
+    name,
+    hierarchy_level)
+values (
+    'member',
+    2);
+
 -- We seed the role_permissions table with the default roles and permissions
 insert into public.role_permissions(
   role,
