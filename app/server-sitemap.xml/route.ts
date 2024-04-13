1d2bc021a15e3fd64af95972ad3f2257ca33b7c5
giancarlo <giancarlopsk@gmail.com>
2024-04-13 12:57:56 +0800

    Refactor key access and role warning in Supabase clients
    The update simplifies accessing environment keys and role warnings in Supabase clients by moving them to new functions 'getSupabaseClientKeys' and 'getServiceRoleKey'. The redundancy in the code is reduced promoting clearer and more maintainable code. The '@epic-web/invariant' import has been removed from files as it is no longer needed.

--- app/server-sitemap.xml/route.ts
+++ app/server-sitemap.xml/route.ts
@@ -1,11 +1,8 @@
-import { invariant } from '@epic-web/invariant';
 import { getServerSideSitemap } from 'next-sitemap';
 
 import { createCmsClient } from '@kit/cms';
 
 import appConfig from '~/config/app.config';
-
-invariant(appConfig.url, 'No NEXT_PUBLIC_SITE_URL environment variable found');
 
 export async function GET() {
   const urls = getSiteUrls();
--- lib/i18n/i18n.resolver.ts
+++ lib/i18n/i18n.resolver.ts
@@ -4,8 +4,8 @@
  */
 export async function i18nResolver(language: string, namespace: string) {
   const data = await import(
-    `../../public/locales/${language}/${namespace}.json`,
-    );
+    `../../public/locales/${language}/${namespace}.json`
+  );
 
   return data as Record<string, string>;
 }
