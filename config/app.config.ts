8362029e54090f421aae08746906047c122bc3ef
giancarlo <giancarlopsk@gmail.com>
2024-04-14 18:50:09 +0800

    Add detailed error messages for missing environment variables
    The commit adds detailed error messages for missing environment variables across different configuration files. It updates the zod schema validations in different files, such as feature-flags.config.ts, personal-accounts-server-actions.ts, and others to provide more informative error messages when environment variables are not provided.

--- config/app.config.ts
+++ config/app.config.ts
@@ -7,22 +7,30 @@ const AppConfigSchema = z
     name: z
       .string({
         description: `This is the name of your SaaS. Ex. "Makerkit"`,
+        required_error: `Please provide the variable NEXT_PUBLIC_PRODUCT_NAME`,
       })
       .min(1),
     title: z
       .string({
         description: `This is the default title tag of your SaaS.`,
+        required_error: `Please provide the variable NEXT_PUBLIC_SITE_TITLE`,
       })
       .min(1),
     description: z.string({
       description: `This is the default description of your SaaS.`,
+      required_error: `Please provide the variable NEXT_PUBLIC_SITE_DESCRIPTION`,
     }),
-    url: z.string().url({
-      message: `Please provide a valid URL. Example: 'https://example.com'`,
-    }),
+    url: z
+      .string({
+        required_error: `Please provide the variable NEXT_PUBLIC_SITE_URL`,
+      })
+      .url({
+        message: `Please provide a valid URL. Example: 'https://example.com'`,
+      }),
     locale: z
       .string({
         description: `This is the default locale of your SaaS.`,
+        required_error: `Please provide the variable NEXT_PUBLIC_DEFAULT_LOCALE`,
       })
       .default('en'),
     theme: z.enum(['light', 'dark', 'system']),
