2fd009c9932110dbd6bc4cccad4ea419958f7cf2
giancarlo <giancarlopsk@gmail.com>
2024-04-16 22:17:28 +0800

    Update database types and relationships
    The commit removes semicolons at the ends of lines and makes updates to the Database object in the `database.types.ts` file. This better aligns the syntax with TypeScript norms. It also affects many database relationships, including but not limited to `Accounts`, `Roles`, and `Subscriptions`.

--- app/(dashboard)/home/_components/personal-account-dropdown-container.tsx
+++ app/(dashboard)/home/_components/personal-account-dropdown-container.tsx
@@ -9,27 +9,37 @@ import { useUser } from '@kit/supabase/hooks/use-user';
 import featuresFlagConfig from '~/config/feature-flags.config';
 import pathsConfig from '~/config/paths.config';
 
+const paths = {
+  home: pathsConfig.app.home,
+};
+
+const features = {
+  enableThemeToggle: featuresFlagConfig.enableThemeToggle,
+};
+
 export function ProfileAccountDropdownContainer(props: {
   collapsed: boolean;
   user: User | null;
+
+  account?: {
+    id: string | null;
+    name: string | null;
+    picture_url: string | null;
+  };
 }) {
   const signOut = useSignOut();
   const user = useUser(props.user);
-
   const userData = user.data ?? props.user ?? null;
 
   return (
-    <div className={props.collapsed ? '' : 'w-full animate-in fade-in-90'}>
+    <div className={props.collapsed ? '' : 'w-full'}>
       <PersonalAccountDropdown
-        paths={{
-          home: pathsConfig.app.home,
-        }}
-        features={{
-          enableThemeToggle: featuresFlagConfig.enableThemeToggle,
-        }}
         className={'w-full'}
+        paths={paths}
+        features={features}
         showProfileName={!props.collapsed}
         user={userData}
+        account={props.account}
         signOutRequested={() => signOut.mutateAsync()}
       />
     </div>
