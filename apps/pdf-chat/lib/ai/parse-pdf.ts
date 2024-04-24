ed538a87f6ab9ff230095b2fedffa0c2f5828f47
giancarlo <giancarlopsk@gmail.com>
2024-04-25 01:33:13 +0700

    Created subtree

--- apps/pdf-chat/lib/ai/parse-pdf.ts
+++
@@ -1,8 +0,0 @@
-import { extractText, getDocumentProxy } from 'unpdf';
-
-export default async function parsePdf(data: ArrayBuffer) {
-  const pdf = await getDocumentProxy(new Uint8Array(data));
-  const { text } = await extractText(pdf, { mergePages: true });
-
-  return text;
-}
