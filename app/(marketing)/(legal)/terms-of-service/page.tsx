5148f09c276bbc08bd4524a6e82a618cb5dd5f0f
giancarlo <giancarlopsk@gmail.com>
2024-04-25 01:33:36 +0700

    Squashed 'apps/pdf-chat/' content from commit 5c13aa2
    git-subtree-dir: apps/pdf-chat
    git-subtree-split: 5c13aa28fb6baa033238f9f3d2c5f084529d2a01

---
+++ app/(marketing)/(legal)/terms-of-service/page.tsx
@@ -0,0 +1,28 @@
+import { SitePageHeader } from '~/(marketing)/_components/site-page-header';
+import { createI18nServerInstance } from '~/lib/i18n/i18n.server';
+import { withI18n } from '~/lib/i18n/with-i18n';
+
+export async function generateMetadata() {
+  const { t } = await createI18nServerInstance();
+
+  return {
+    title: t('marketing:termsOfService'),
+  };
+}
+
+async function TermsOfServicePage() {
+  const { t } = await createI18nServerInstance();
+
+  return (
+    <div className={'mt-8'}>
+      <div className={'container mx-auto'}>
+        <SitePageHeader
+          title={t(`marketing:termsOfService`)}
+          subtitle={t(`marketing:termsOfServiceDescription`)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default withI18n(TermsOfServicePage);
