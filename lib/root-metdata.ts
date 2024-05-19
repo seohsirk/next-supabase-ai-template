6fa17c6160315595e0dbc49f77dc739097c802f0
giancarlo <giancarlopsk@gmail.com>
2024-05-19 23:51:37 +0700

    Squashed 'apps/chatbots/' changes from f1395ff..d8692db
    d8692db Merge branch 'main' of github.com:makerkit/next-supabase-saas-kit-turbo
    7b4c671 Add AuthCallbackService to handle auth callbacks in Supabase (#25)
    
    git-subtree-dir: apps/chatbots
    git-subtree-split: d8692db23371370b8194dd34a76a13c99f3d07c9

--- lib/root-metdata.ts
+++ lib/root-metdata.ts
@@ -1,27 +1,37 @@
 import { Metadata } from 'next';
 
+import { headers } from 'next/headers';
+
 import appConfig from '~/config/app.config';
 
 /**
- * @name rootMetadata
- * @description Define the root metadata for the application.
+ * @name generateRootMetadata
+ * @description Generates the root metadata for the application
  */
-export const rootMetadata: Metadata = {
-  title: appConfig.name,
-  description: appConfig.description,
-  metadataBase: new URL(appConfig.url),
-  openGraph: {
-    url: appConfig.url,
-    siteName: appConfig.name,
+export const generateRootMetadata = (): Metadata => {
+  const csrfToken = headers().get('x-csrf-token') ?? '';
+
+  return {
+    title: appConfig.name,
     description: appConfig.description,
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: appConfig.title,
-    description: appConfig.description,
-  },
-  icons: {
-    icon: '/images/favicon/favicon.ico',
-    apple: '/images/favicon/apple-touch-icon.png',
-  },
+    metadataBase: new URL(appConfig.url),
+    applicationName: appConfig.name,
+    other: {
+      'csrf-token': csrfToken,
+    },
+    openGraph: {
+      url: appConfig.url,
+      siteName: appConfig.name,
+      description: appConfig.description,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: appConfig.title,
+      description: appConfig.description,
+    },
+    icons: {
+      icon: '/images/favicon/favicon.ico',
+      apple: '/images/favicon/apple-touch-icon.png',
+    },
+  };
 };
