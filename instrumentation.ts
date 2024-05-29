38213ddc727d4a88d5de99c7e5a430bdbeed771a
giancarlo <giancarlopsk@gmail.com>
2024-05-29 09:48:06 +0700

    Squashed 'apps/ai-avatars/' changes from 7efb80a..699c9ac
    699c9ac Merge branch 'main' of github.com:makerkit/next-supabase-saas-kit-turbo
    5f46f53 Add user id parameter to multi-factor authentication functions
    1125e92 Merge branch 'main' of github.com:makerkit/next-supabase-saas-kit-turbo
    aeebcb3 Sentry + Turbopack fixes (#28)
    b90d63a Updated packages
    b9c968b Upgrade dependencies and enhance UI components
    ce601e2 xMerge branch 'main' of github.com:makerkit/next-supabase-saas-kit-turbo
    2522da0 Force no cache in Supabase clients with Next.js (#27)
    c5c4cb7 Updated packages
    9c2d1e3 Merge branch 'main' of github.com:makerkit/next-supabase-saas-kit-turbo
    f1b9eca Update error handling and function calls in billing services
    
    git-subtree-dir: apps/ai-avatars
    git-subtree-split: 699c9ac2a5a399c570a79e5ba6110168ff8763f9

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,18 +2,12 @@
  * This file is used to register monitoring instrumentation
  * for your Next.js application.
  */
-
 export async function register() {
-  // only run in nodejs runtime
-  if (
-    process.env.NEXT_RUNTIME === 'nodejs' &&
-    process.env.MONITORING_INSTRUMENTATION_ENABLED === 'true'
-  ) {
-    const { registerMonitoringInstrumentation } = await import(
-      '@kit/monitoring/instrumentation'
-    );
+  const { registerMonitoringInstrumentation } = await import(
+    '@kit/monitoring/instrumentation'
+  );
 
-    // Register monitoring instrumentation based on the MONITORING_PROVIDER environment variable.
-    return registerMonitoringInstrumentation();
-  }
+  // Register monitoring instrumentation
+  // based on the MONITORING_PROVIDER environment variable.
+  await registerMonitoringInstrumentation();
 }
