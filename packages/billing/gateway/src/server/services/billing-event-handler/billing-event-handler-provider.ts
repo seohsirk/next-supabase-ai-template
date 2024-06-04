2696c08d7d0f3b70bb0a2a7a9214828138f08a91
giancarlo <giancarlopsk@gmail.com>
2024-06-04 13:32:02 +0700

    Refactor BillingEventHandlerService and update SQL permissions
    Changed BillingEventHandlerService from a class to a method. Also, the SQL permissions for service_role on public.order_items table have been updated to include insert, update and delete operations. Additionally, made adjustment to configuration values in the billing.sample.config.ts file.

--- packages/billing/gateway/src/server/services/billing-event-handler/billing-event-handler-provider.ts
+++ packages/billing/gateway/src/server/services/billing-event-handler/billing-event-handler-provider.ts
@@ -5,16 +5,23 @@ import { Database } from '@kit/supabase/database';
 import { getSupabaseServerActionClient } from '@kit/supabase/server-actions-client';
 
 import { BillingEventHandlerFactoryService } from './billing-event-handler-factory.service';
-import { BillingEventHandlerService } from './billing-event-handler.service';
+import { createBillingEventHandlerService } from './billing-event-handler.service';
+
+// a function that returns a Supabase client
+type ClientProvider = () => ReturnType<typeof getSupabaseServerActionClient>;
+
+// the billing provider from the database
+type BillingProvider = Database['public']['Enums']['billing_provider'];
 
 /**
+ * @name getBillingEventHandlerService
  * @description This function retrieves the billing provider from the database and returns a
  * new instance of the `BillingGatewayService` class. This class is used to interact with the server actions
  * defined in the host application.
  */
 export async function getBillingEventHandlerService(
-  clientProvider: () => ReturnType<typeof getSupabaseServerActionClient>,
-  provider: Database['public']['Enums']['billing_provider'],
+  clientProvider: ClientProvider,
+  provider: BillingProvider,
   config: BillingConfig,
 ) {
   const strategy = await BillingEventHandlerFactoryService.GetProviderStrategy(
@@ -22,5 +29,5 @@ export async function getBillingEventHandlerService(
     config,
   );
 
-  return new BillingEventHandlerService(clientProvider, strategy);
+  return createBillingEventHandlerService(clientProvider, strategy);
 }
