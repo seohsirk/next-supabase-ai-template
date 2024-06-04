8808e3755830bd89c5ecba71dffd3f881f3239b8
giancarlo <giancarlopsk@gmail.com>
2024-06-04 19:38:51 +0700

    Refactor sign out and auth change listener hooks
    In the use-sign-out.ts file, the use of query client has been removed and error handling has been improved during sign-out operations. Meanwhile, in the use-auth-change-listener.ts file, an unnecessary usage of query client and router has been removed. Event handling logic has also been simplified by using window.location.reload() instead of router.refresh() on user sign-out.

--- packages/supabase/src/hooks/use-sign-out.ts
+++ packages/supabase/src/hooks/use-sign-out.ts
@@ -1,15 +1,13 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { useSupabase } from './use-supabase';
 
 export function useSignOut() {
   const client = useSupabase();
-  const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async () => {
-      await client.auth.signOut();
-      await queryClient.invalidateQueries();
+    mutationFn: () => {
+      return client.auth.signOut();
     },
   });
 }
