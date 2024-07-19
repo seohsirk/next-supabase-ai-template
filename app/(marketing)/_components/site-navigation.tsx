fb7021698905e931c3dd628415f848563d000749
gbuomprisco <giancarlopsk@gmail.com>
2024-07-19 12:23:50 +0200

    Squashed 'apps/ai-avatars/' changes from 606f242b..0c2f6f0a
    0c2f6f0a Merge branch 'main' of github.com:makerkit/next-supabase-saas-kit-turbo
    da20301d Update site navigation styles and cleanup page imports
    c7013f36 Update CSS classes for multiple components
    4ef7ce06 Merge branch 'main' of github.com:makerkit/next-supabase-saas-kit-turbo
    0b704d9e Update visuals and improve readability on marketing pages
    
    git-subtree-dir: apps/ai-avatars
    git-subtree-split: 0c2f6f0a2408245081696acca8914dbe10b2f3f8

--- app/(marketing)/_components/site-navigation.tsx
+++ app/(marketing)/_components/site-navigation.tsx
@@ -53,7 +53,7 @@ export function SiteNavigation() {
             'rounded-full border border-gray-100 px-4 py-2 dark:border-primary/10'
           }
         >
-          <NavigationMenuList className={'space-x-1'}>
+          <NavigationMenuList className={'space-x-4'}>
             {NavItems}
           </NavigationMenuList>
         </NavigationMenu>
