e23489d3086654246a5b797b62a90b9f673ce6c9
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2024-08-30 20:39:10 +0800

    Fix RLS Policy for Storage preventing non-uuid file names (#57)

--- apps/web/supabase/migrations/20221215192558_schema.sql
+++ apps/web/supabase/migrations/20221215192558_schema.sql
@@ -2722,25 +2722,22 @@ grant
 execute on function kit.get_storage_filename_as_uuid (text) to authenticated,
 service_role;
 
--- RLS policies for storage
+-- RLS policies for storage bucket account_image
 create policy account_image on storage.objects for all using (
   bucket_id = 'account_image'
-  and kit.get_storage_filename_as_uuid (name) = (
-    select
-      auth.uid ()
+  and (
+    kit.get_storage_filename_as_uuid(name) = auth.uid()
+    or public.has_role_on_account(kit.get_storage_filename_as_uuid(name))
   )
-  or public.has_role_on_account (kit.get_storage_filename_as_uuid (name))
 )
-with
-  check (
-    bucket_id = 'account_image'
-    and (kit.get_storage_filename_as_uuid (name) = (
-      select
-        auth.uid ()
-    )
-    or public.has_permission (
-      auth.uid (),
-      kit.get_storage_filename_as_uuid (name),
+with check (
+  bucket_id = 'account_image'
+  and (
+    kit.get_storage_filename_as_uuid(name) = auth.uid()
+    or public.has_permission(
+      auth.uid(),
+      kit.get_storage_filename_as_uuid(name),
       'settings.manage'
-    ))
-  );
+    )
+  )
+);
